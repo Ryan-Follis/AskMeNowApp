d4e3bdfd8eedf7f3395c51fbe312c158788157dc
gon-kyo <64513228+gon-kyo@users.noreply.github.com>
2023-04-03 17:03:54 -0500

    final push today

--- app/src/main/java/com/example/askmenow/userProfileActivity.java
+++ app/src/main/java/com/example/askmenow/userProfileActivity.java
@@ -1,15 +1,32 @@
 package com.example.askmenow;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class userProfileActivity extends AppCompatActivity {
     private static String[]  data = new String[6];
+    private static int visibility = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inf = getMenuInflater();
+        inf.inflate(R.menu.visibility_dropdown, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        return true;
     }
 
     public int push(String field){
