2fe47050b13f16dccd6cf549cf7312286eb68f86
gon-kyo <64513228+gon-kyo@users.noreply.github.com>
2023-04-05 17:32:19 -0500

    drop box done

--- app/src/main/java/com/example/askmenow/userProfileActivity.java
+++ app/src/main/java/com/example/askmenow/userProfileActivity.java
@@ -7,14 +7,33 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
 public class userProfileActivity extends AppCompatActivity {
     private static String[]  data = new String[6];
     private static int visibility = 0;
+    String[] dropdownMenu = {"Everyone", "Friends Only", "Only Me"};
+    AutoCompleteTextView auto;
+    ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+        auto = findViewById(R.id.autodrop);
+        adapter = new ArrayAdapter<String>(this,R.layout.list, dropdownMenu);
+        auto.setAdapter(adapter);
+        auto.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String item = adapterView.getItemAtPosition(i).toString();
+                Toast.makeText(userProfileActivity.this, item,Toast.LENGTH_SHORT).show();
+                visibility = i;
+            }
+        });
     }
 
     @Override
