1af47305cd62521e51fe3ae78138daacebd3a594
gon-kyo <64513228+gon-kyo@users.noreply.github.com>
2023-04-12 17:35:36 -0500

    firebase setup

--- app/src/main/java/com/example/askmenow/userProfileActivity.java
+++ app/src/main/java/com/example/askmenow/userProfileActivity.java
@@ -3,10 +3,8 @@ package com.example.askmenow;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -14,12 +12,15 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
 public class userProfileActivity extends AppCompatActivity {
     private static String[]  data = new String[6];
     private static int visibility = -1;
     String[] dropdownMenu = {"Everyone", "Friends Only", "Only Me"};
     AutoCompleteTextView auto;
     ArrayAdapter<String> adapter;
+    private AlertDialog.Builder dBuilder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,7 +49,13 @@ public class userProfileActivity extends AppCompatActivity {
         });
 
         //Listener for change password
-
+        Button changePassword = findViewById(R.id.changepassword);
+        changePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            
+            }
+        });
 
 
     }
@@ -80,5 +87,4 @@ public class userProfileActivity extends AppCompatActivity {
         visibility = -1;
         return 0;
     }
-
 }
