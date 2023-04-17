8240fd5a2cccea962f3cce0741fc33600b54652b
gon-kyo <64513228+gon-kyo@users.noreply.github.com>
2023-04-17 17:54:04 -0500

    update

--- app/src/main/java/com/example/askmenow/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/askmenow/ui/profile/ProfileFragment.java
@@ -1,6 +1,41 @@
 package com.example.askmenow.ui.profile;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.askmenow.R;
+import com.example.askmenow.databinding.FragmentDmsBinding;
+import com.example.askmenow.databinding.FragmentProfileBinding;
+import com.example.askmenow.ui.dms.DMsViewModel;
 
 public class ProfileFragment extends Fragment {
+    private FragmentProfileBinding binding;
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        ProfileViewModel homeViewModel =
+                new ViewModelProvider(this).get(ProfileViewModel.class);
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.
+        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
 }
