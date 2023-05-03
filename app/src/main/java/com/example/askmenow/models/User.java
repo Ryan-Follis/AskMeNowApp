d7501623f6b80dd265475e84b307e45262167677
gbgre <gbgreenberg2@gmail.com>
2023-05-02 19:59:40 -0500

    Added location checker/reputation file

--- app/src/main/java/com/example/askmenow/models/User.java
+++ app/src/main/java/com/example/askmenow/models/User.java
@@ -6,6 +6,8 @@ import java.util.List;
 public class User implements Serializable{
     public String name, username, image, email, token, id;
     public int age;
+    public double longitude,latitude;
+    public int[] weekScore;
     public List<String> interests;
 }
 
